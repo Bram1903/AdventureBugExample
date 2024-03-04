1a527f79b0a917f700f4dc9280673e1bce5241a6
Bram <70259613+Bram1903@users.noreply.github.com>
2024-03-04 12:48:47 +0100

    Added: Welcomer Base

--- src/main/java/me/gregzee/welcomer/Welcomer.java
+++ src/main/java/me/gregzee/welcomer/Welcomer.java
@@ -1,27 +1,40 @@
-package com.deathmotion.testplugin;
+package me.gregzee.welcomer;
 
 import co.aikar.commands.PaperCommandManager;
-import com.deathmotion.testplugin.commands.TestCommand;
 import lombok.Getter;
+import me.gregzee.welcomer.managers.StartupManager;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
-public class TestPlugin extends JavaPlugin {
+public class Welcomer extends JavaPlugin {
+
     private BukkitAudiences adventure;
     private PaperCommandManager commandManager;
 
     @Override
     public void onEnable() {
         adventure = BukkitAudiences.create(this);
-
         commandManager = new PaperCommandManager(this);
-        commandManager.registerCommand(new TestCommand(this));
+
+        new StartupManager(this);
+
+        enableBStats();
     }
 
     @Override
     public void onDisable() {
         adventure.close();
         getLogger().info("Plugin has been uninitialized!");
+    }
+
+    private void enableBStats() {
+        try {
+            // TODO: Replace 1 with your resource id
+            new Metrics(this, 1);
+        } catch (Exception e) {
+            getLogger().warning("Something went wrong while enabling bStats.\n" + e.getMessage());
+        }
     }
 }
--- src/main/java/me/gregzee/welcomer/commands/TestCommand.java
+++ src/main/java/me/gregzee/welcomer/commands/TestCommand.java
@@ -1,22 +1,23 @@
-package com.deathmotion.testplugin.commands;
+package me.gregzee.welcomer.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Default;
-import com.deathmotion.testplugin.TestPlugin;
+import me.gregzee.welcomer.Welcomer;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
 
 @CommandAlias("test")
 public class TestCommand extends BaseCommand {
     private final BukkitAudiences adventure;
 
-    public TestCommand(TestPlugin plugin) {
+    public TestCommand(Welcomer plugin) {
         this.adventure = plugin.getAdventure();
     }
 
     @Default
     public void test(CommandSender sender) {
-        sender.sendMessage("Hello, world!");
+        adventure.sender(sender).sendMessage(Component.text("Hello, world!"));
     }
 }
